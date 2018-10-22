d7f181fe2f361a9b7d47cf46807932097c365306
Shayan Zadeh <shayan@zadeh.us>
2018-10-21 20:24:14 -0500

    supporting marathon 1.6 binary structure

--- Berksfile
+++ Berksfile
@@ -1,6 +1,6 @@
-#!/usr/bin/env ruby
-# ^syntax detection
-
 source 'https://supermarket.chef.io'
+source :chef_server
 
 metadata
+
+cookbook 'mesos', git: 'git@github.com:leaprail/mesos_cookbook.git', branch: 'systemd-env'
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
 default['java']['jdk_version']            = '8'
 
 # Marathon package
-default['marathon']['version']            = '1.3.5'
+default['marathon']['version']            = '1.6.322-2bf46b341'
 default['marathon']['source']['url']      =
-  "http://downloads.mesosphere.com/marathon/v#{node['marathon']['version']}/marathon-#{node['marathon']['version']}.tgz"
+  "https://downloads.mesosphere.com/marathon/releases/#{node['marathon']['version'].split('-').first}/marathon-#{node['marathon']['version']}.tgz"
 default['marathon']['source']['checksum'] = '298723dd54fd8c65c4cd9a052ca632a21e2d95133e328370fe2edede94a1804e'
 default['marathon']['syslog']             = true
 
